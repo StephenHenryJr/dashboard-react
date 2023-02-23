5fb0f0728fb8954d6c8f021022f41c04b8148b64
StephenHenryJr <shenryjr_85@hotmail.com>
2023-02-22 22:51:59 -0500

    Added stacked chart

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -21,7 +21,6 @@ import { useStateContext } from "../contexts/ContextProvider";
 const Ecommerce = () => {
   return (
     <div className="mt-12">
-
       {/* First Section */}
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
         {/* Earnings */}
@@ -41,7 +40,8 @@ const Ecommerce = () => {
               size="md"
             />
           </div>
-        </div>
+        </div>{/* Earnings */}
+        
 
         {/* Cards */}
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
@@ -66,9 +66,80 @@ const Ecommerce = () => {
               <p className="text-sm text-gray-400  mt-1">{item.title}</p>
             </div>
           ))}
-        </div>
-      </div>
+        </div> {/* Cards */}    
+      </div>{/* First Section */}
+      
 
+      {/* Second Section */}
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780  ">
+          {/* Revenue */}
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>{/* Revenue */}
+          
+
+          {/* Budget Expense */}
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            {/* Left Side */}
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p className="text-3xl font-semibold">$48,487</p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkLine"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>  
+            </div>
+            {/* Right Side */}
+            <div>
+              <Stacked width="320px" height="360px"/>
+            </div>            
+          </div>{/* Budget Expense */}
+          
+
+        </div>
+      </div>{/* Second Section */}
+      
     </div>
   );
 };
